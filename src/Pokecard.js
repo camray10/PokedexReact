e28b093a14c233505c8bf10bc2b9f3fdffbc2283
Cameron Ray <rayc287@protonmail.com>
2023-01-25 15:02:28 -0500

    Pokedex React Exercise

---
+++ src/Pokecard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Pokecard.css";
+
+const POKEMON_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+
+/** Individual Pokemon card.
+ *
+ * Props:
+ * - title
+ * - image
+ * - type
+ * - exp: (number of experience points)
+ *
+ * */
+
+function Pokecard({ id, name, type, exp }) {
+  const imgSrc = `${POKEMON_API}${id}.png`;
+
+  return (
+      <div className="Pokecard">
+        <div className="Pokecard-title">{name}</div>
+        <img className="Pokecard-image" src={imgSrc} alt={name} />
+        <div className="Pokecard-data">Type: {type}</div>
+        <div className="Pokecard-data">EXP: {exp}</div>
+      </div>
+  );
+}
+
+export default Pokecard;
